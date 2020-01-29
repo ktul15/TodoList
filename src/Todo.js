0b4871e3c4fccc2a073e3bfd37029eba2939a461
Ktul <ketulm8@gmail.com>
2020-01-29 11:32:32 +0530

    Add Todo Component

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <p>To make a website</p>
+      </div>
+    );
+  }
+}
+
+export default Todo;
---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Todo";
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div>
+        <Todo />
+      </div>
+    );
+  }
+}
+
+export default TodoList;
